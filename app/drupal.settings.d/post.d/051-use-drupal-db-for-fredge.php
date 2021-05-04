49240ae98c5a52e76da8c3b3a74816cdb435f89a
eileen <emcnaughton@wikimedia.org>
2021-05-04 15:21:11 +1200

    Revert "Use drupal db for fredge" and set permissions moar better
    The db commit didn't seem to work so reverting. Added moar better
    very good dastardly clever fix.
    
    This reverts commit 1d72ae7193555f1963e694363e4b77fa411f9855.
    
    Bug: T281609
    Bug: T281647
    Change-Id: I14fcfd3c774ea98593e0f9226c6b5f6ceb47f798

--- app/drupal.settings.d/post.d/051-use-drupal-db-for-fredge.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $databases;
-// Set the fredge db to create tables in the drupal db.
-// This is a temporary step until we can sort out
-// the grant permissions.
-$databases['fredge']['default'] = $databases['default']['default'];
